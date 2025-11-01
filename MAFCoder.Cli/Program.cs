1ea46154ed92a3aa8a6a255aae74fa52dede3440
Velvárt András <andras.velvart@response.hu>
2025-11-01 03:56:07 +0100

    moved to MAFCoder.CLI

--- MAFCoder.Cli/Program.cs
+++ MAFCoder.Cli/Program.cs
@@ -10,7 +10,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 AnsiConsole.Background = Color.Black;
 AnsiConsole.Clear();
-AnsiConsole.Write(new FigletText("MAFCoder").Centered());
+AnsiConsole.Write(new FigletText("MAFCoder.CLI").Centered());
 
 var key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
 
